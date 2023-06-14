52d075b5db5755b0eb84bfeb4ad3b1fcfce2c2bd
Sebastian Olivares <sebasfn9710dev@gmail.com>
2023-06-14 07:21:44 -0500

    Add docker container files and fastAPI app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run_v1():
@@ -21,6 +22,20 @@ def run_v2():
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_charts(countries, percentages)
 
+def run_pandas():
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_charts(countries, percentages)
+
+  data = read_csv.read_csv('data.csv')
+  country = input('Type Country => ')
+  result = utils.population_by_country(data, country)
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
 
 if __name__ == "__main__":
-  run_v1()
+  run_pandas()
